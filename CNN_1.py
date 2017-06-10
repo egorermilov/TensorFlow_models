ef6f4f14ce0f8669db1ffc4cf5924b3feff3a49c
Egor Ermilov <egor.e.ermilov@gmail.com>
2017-06-10 12:52:15 +0300

    Add files via upload

--- CNN_1.py
+++ CNN_1.py
@@ -26,6 +26,8 @@ max_pool_size1 = 2
 max_pool_size2 = 2
 fully_connected_size1 = 100
 
+sess = tf.Session()
+
 x_input_shape = (
     batch_size,
     image_width,
@@ -34,6 +36,7 @@ x_input_shape = (
 )
 x_input = tf.placeholder(tf.float32, shape=x_input_shape)
 y_input = tf.placeholder(tf.float32, shape=(batch_size))
+y_target = tf.placeholder(tf.int32, shape=(batch_size))
 
 eval_input_shape = (
     evaluation_size,
@@ -111,7 +114,127 @@ full2_bias = tf.Variable(
 def my_conv_net(input_data):
     conv1 = tf.nn.conv2d(
         input_data,
+		conv1_weight,
         strides=[1, 1, 1, 1],
         padding="SAME"
     )
-    
+    relu1 = tf.nn.relu(
+        tf.nn.bias_add(
+            conv1,
+            conv1_bias
+        )
+    )
+    max_pool1 = tf.nn.max_pool(
+        relu1,
+        ksize=[1, max_pool_size1, max_pool_size1, 1],
+        strides=[1, max_pool_size1, max_pool_size1, 1],
+        padding="SAME"
+    )
+
+    conv2 = tf.nn.conv2d(
+        max_pool1,
+        conv2_weight,
+        strides=[1, 1, 1, 1],
+        padding="SAME"
+    )
+    relu2 = tf.nn.relu(
+        tf.nn.bias_add(
+            conv2,
+            conv2_bias
+        )
+    )
+    max_pool2 = tf.nn.max_pool(
+        relu2,
+        ksize=[1, max_pool_size2, max_pool_size2, 1],
+        strides=[1, max_pool_size2, max_pool_size2, 1],
+        padding="SAME"
+    )
+
+    final_conv_shape = max_pool2.get_shape().as_list()
+    final_shape = final_conv_shape[1] * final_conv_shape[2] * final_conv_shape[3]
+    flat_output = tf.reshape(max_pool2, [final_conv_shape[0], final_shape])
+
+    fully_connected1 = tf.nn.relu(
+        tf.add(
+            tf.matmul(flat_output, full1_weight),
+            full1_bias
+        )
+    )
+    final_model_output = tf.add(
+        tf.matmul(
+            fully_connected1,
+            full2_weight
+        ),
+        full2_bias
+    )
+    return final_model_output
+
+model_output = my_conv_net(x_input)
+test_model_output = my_conv_net(eval_input)
+
+loss = tf.reduce_mean(
+    tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=model_output,
+        labels=y_target
+    )
+)
+
+prediction = tf.nn.softmax(model_output)
+test_prediction = tf.nn.softmax(test_model_output)
+
+def get_accuracy(logits, targets):
+	batch_predictions = np.argmax(logits, axis=1)
+	num_correct = np.sum(np.equal(batch_predictions,targets))
+	return num_correct / batch_predictions.shape[0]
+
+my_optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
+train_step = my_optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+train_loss = []
+train_acc = []
+test_acc = []
+
+for i in range(generations):
+	rand_index = np.random.choice(len(train_xdata), batch_size)
+	rand_x = train_xdata[rand_index]
+	rand_x = np.expand_dims(rand_x, 3)
+	rand_y = train_labels[rand_index]
+	train_dict = {x_input: rand_x, y_target: rand_y}
+
+	sess.run(train_step, feed_dict=train_dict)
+
+	temp_train_loss, temp_train_preds = sess.run(
+		[loss, prediction],
+		feed_dict=train_dict
+	)
+	temp_train_acc = get_accuracy(temp_train_preds, rand_y)
+	print(
+		"Epoch: {}, train loss: {}, train accuracy: {}".format(
+			i,
+			temp_train_loss,
+			temp_train_acc
+		)
+	)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
